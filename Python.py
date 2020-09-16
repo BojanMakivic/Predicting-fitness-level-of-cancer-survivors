c6a9fb1cf65494a15e449ed26461a3217561567e
Bojan Makivic <59933743+BojanMakivic@users.noreply.github.com>
2020-09-17 01:07:06 +0200

    Update Python.py

--- Python.py
+++ Python.py
@@ -147,8 +147,82 @@ print ("RMSE is: ",SVR_rmse) # RMSE is:  32.615674028371714 -- We can see that t
 
 ## Random forest model
 
+from sklearn.ensemble import RandomForestRegressor
 
+forest_reg = RandomForestRegressor(n_estimators=100, random_state=42, oob_score=True)
+forest_reg.fit(train_prepared, train_labels)
 
+# RMSE for random forest model
+
+watt_RF_predictions = forest_reg.predict(train_prepared)
+forest_mse = mean_squared_error(train_labels, watt_RF_predictions)
+forest_rmse = np.sqrt(forest_mse)
+print("RMSE is: ", forest_rmse) # RMSE is:  13.078592962697485 -- We can see that the prediction error for linear model is 13.1 Wat (until now the model with lowest prediction error)
+
+## Fine tuning of models
+
+'''The following code randomly splits the training set into 10 distinct subsets called folds, then it trains and evaluates the model 10 times, 
+picking a different fold for evaluation every time and training on the other 9 folds. The result is an array containing the 10 evaluation scores:'''
+
+from sklearn.model_selection import cross_val_score
+
+# Function to display the score statistics
+
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Standard deviation:", scores.std())
+    
+# Linear model
+
+lin_scores = cross_val_score(lin_reg, train_prepared, train_labels,
+                             scoring="neg_mean_squared_error", cv=10)
+lin_rmse_scores = np.sqrt(-lin_scores)
+display_scores(lin_rmse_scores)
+
+''' Scores: [30.53455934 27.46985412 38.43562164 32.76486023 38.07501257 31.3886864531.97227142 31.11989247 32.31836557 30.94683766]
+    Mean: 32.50259614886735
+    Standard deviation: 3.184513995300587'''
+
+# SVR
+
+SVR_scores = cross_val_score(svm_reg, train_prepared, train_labels,
+                                scoring="neg_mean_squared_error", cv=10)
+SVR_rmse_scores = np.sqrt(-SVR_scores)
+display_scores(SVR_rmse_scores)
+
+''' Scores: [31.19126727 27.5297865  38.9271332  33.18049647 38.51193615 31.0531404231.92811072 30.98819848 32.00674191 30.15057619]
+    Mean: 32.54673873052626
+    Standard deviation: 3.387883572916082'''
+
+# Random forest
+
+forest_scores = cross_val_score(forest_reg, train_prepared, train_labels,
+                                scoring="neg_mean_squared_error", cv=10)
+forest_rmse_scores = np.sqrt(-forest_scores)
+display_scores(forest_rmse_scores)
+
+'''Scores: [34.41167315 29.82213183 40.39247186 34.37520891 36.24074161 31.6744174434.49259181 31.78694634 33.58427049 33.96558781]
+   Mean: 34.07460412488601
+   Standard deviation: 2.7261035077436997'''
+
+# Return the table of cross-validation for all three models
+
+scores = cross_val_score(lin_reg,  train_prepared, train_labels, scoring="neg_mean_squared_error", cv=10)
+scores_rf = cross_val_score(forest_reg,  train_prepared, train_labels, scoring="neg_mean_squared_error", cv=10)
+scores_svr = cross_val_score(svm_reg,  train_prepared, train_labels, scoring="neg_mean_squared_error", cv=10)
+a = pd.Series(np.sqrt(-scores)).describe()
+b = pd.Series(np.sqrt(-scores_svr)).describe()
+c = pd.Series(np.sqrt(-scores_rf)).describe()
+scores_df = pd.DataFrame({
+    "lin_reg": a,
+    "SVR":b,
+    "forest_reg": c
+    
+})
+scores_df
+
+# Cross-validation table
 '''	     lin_reg	forest_reg	SVR
 count	10.000000	10.000000	10.000000
 mean	32.502596	33.978674	32.546739
